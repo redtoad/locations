a908ea04009cbc1816bdd28b93aae387fa15c82a
Sebastian Rahlf <basti@redtoad.de>
2015-01-05 22:52:34 +0100

    Add generic properties field.

--- src/models.py
+++ src/models.py
@@ -11,7 +11,16 @@ class Location(db.Document):
     address = db.StringField()
     point = db.PointField()
 
+    properties = db.DictField()
+
     timestamp = db.DateTimeField(default=datetime.datetime.now())
+
+    def __repr__(self):
+        return '<Location address=%r, point=%r>' % (
+            self.address, self.point)
+
+    def __unicode__(self):
+        return repr(self)
 
 
 # http://gis.stackexchange.com/a/101898
